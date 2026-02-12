032c8247602b1ce55bc9e606bd721b05ebcf54dc
Znerzi <diazrenz18@gmail.com>
2026-02-12 14:29:45 +0800

    about with logo

--- view/about.php
+++ view/about.php
@@ -13,6 +13,7 @@
   <!-- Bootstrap & Icons -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+  <link rel="icon" type="image/png" href="/csnk/resources/img/SVG-preview.png">
 </head>
 <body class="bg-light">
 
