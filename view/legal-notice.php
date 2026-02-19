2fcfdae7bd92196c2ea4fab4422cd6456163c402
Znerzi <diazrenz18@gmail.com>
2026-02-19 09:20:32 +0800

    fixed icon

--- view/legal-notice.php
+++ view/legal-notice.php
@@ -8,6 +8,7 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Legal Notice - CSNK Manpower Agency</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link rel="icon" type="image/png" href="/csnk/resources/img/csnk-icon.png">
   <style>
     :root {
       --accent-red: #D72638;
