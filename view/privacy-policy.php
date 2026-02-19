a0768aa84ac91548cd386e757f86d294a319864a
Znerzi <diazrenz18@gmail.com>
2026-02-19 09:20:35 +0800

    fixed icon

--- view/privacy-policy.php
+++ view/privacy-policy.php
@@ -10,6 +10,7 @@ session_start();
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+  <link rel="icon" type="image/png" href="/csnk/resources/img/csnk-icon.png">
   <style>
     :root {
       --accent-red: #D72638;
