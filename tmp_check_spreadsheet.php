47f9bd84548d024fe3d31d6245bd016142b93344
Znerzi <diazrenz18@gmail.com>
2026-03-06 13:42:40 +0800

    deleted temporary files

--- tmp_check_spreadsheet.php
+++
@@ -1,4 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-echo 'CacheInterface: ' . (interface_exists('Psr\\SimpleCache\\CacheInterface') ? 'yes' : 'no') . PHP_EOL;
-echo 'Spreadsheet: ' . (class_exists('PhpOffice\\PhpSpreadsheet\\Spreadsheet') ? 'yes' : 'no') . PHP_EOL;
