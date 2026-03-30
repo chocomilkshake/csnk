9c56e358eeac3e528b05300c4385ac0794eb4b8e
Znerzi <diazrenz18@gmail.com>
2026-03-30 13:19:13 +0800

    added email app password of SMC Email

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -24,6 +24,25 @@ define('UPLOAD_PATH', __DIR__ . '/../uploads/');
 define('UPLOAD_URL', APP_URL . '/uploads/');
 
 // Replacement uploads
+define('REPLACEMENTS_UPLOAD_SUBDIR', 'replacements');
+define('REPLACEMENTS_UPLOAD_PATH', UPLOAD_PATH . REPLACEMENT
+
+    ini_set('session.cookie_httponly', 1);
+    ini_set('session.use_only_cookies', 1);
+    ini_set('session.cookie_secure', 0); // set to 1 when HTTPS is enabled
+
+    session_set_cookie_params([
+        'lifetime' => 0,
+        'path'     => '/',
+        'domain'   => '',
+        'secure'   => false, // true in production HTTPS
+        'httponly' => true,
+        'samesite' => 'Lax', // use 'Strict' if possible
+    ]);
+
+    session_start();
+}
+
 
 /* ======================================================
    TIMEZONE
