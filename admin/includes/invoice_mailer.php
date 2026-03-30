0beee73cd68627d6bba92e18747e49a81ca13208
Znerzi <diazrenz18@gmail.com>
2026-03-30 10:26:01 +0800

    improved design of billing mailer

--- admin/includes/invoice_mailer.php
+++ admin/includes/invoice_mailer.php
@@ -2,6 +2,23 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+ *
+ * @return bool
+ */
+function sendInvoiceEmail(
+    string $toEmail,
+    string $clientName,
+    string $invoiceNumber,
+    string $pdfPath,
+    string $companyType = 'CSNK',
+    ?string $paymentLink = null
+): bool {
+
+    <table width='620' cellpadding='0' cellspacing='0'
+           style='background:#ffffff;border-radius:14px;
+                  box-shadow:0 12px 36px rgba(0,0,0,0.14);
+                  overflow:hidden;'>
+
         <!-- HEADER -->
         <tr>
             <td style='
