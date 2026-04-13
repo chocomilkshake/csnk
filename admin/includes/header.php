e2452076b19e242773020070b63ca6d5dc3cbce3
Znerzi <diazrenz18@gmail.com>
2026-04-13 09:14:51 +0800

    added security session auto-logout

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -691,6 +691,7 @@ if ($canViewReports && $conn instanceof mysqli) {
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 
+
 <script>
 (() => {
     const IDLE_LIMIT = 300000; // 5 minutes
@@ -711,6 +712,7 @@ if ($canViewReports && $conn instanceof mysqli) {
     window.addEventListener('load', resetIdleTimer);
 })();
 </script>
+
 
 <body>
     <!-- Sidebar -->
